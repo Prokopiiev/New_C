6f202eb101be0788a733aae1111e9675fb2a8af9
Kirill Logoza <logoza.kirill@yandex.ru>
2026-09-19 18:41:13 +0300

    третье задание

--- HW1/A2.c
+++ HW1/A2.c
@@ -26,9 +26,12 @@
 #include <stdlib.h>
 
 int main(void) {
-    printf("Let's\n");
-    printf("	go\n");
-    printf("		to walk\n");
+    printf("     *\n");
+    printf("    ***\n");
+    printf("   *****\n");
+    printf("  *******\n");
+    printf("   HHOHH\n");
+    printf("   ZZZZZ\n");
     system("pause");   
     return 0;
 }
