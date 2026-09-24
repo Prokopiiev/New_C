3c47ee79653df05712dd36e6485af76be0c6d540
Kirill Logoza <logoza.kirill@yandex.ru>
2026-09-24 20:31:36 +0300

    задание пятнадцать

---
+++ HW1/A14.c
@@ -0,0 +1,45 @@
+/*
+ * TEST.c
+ * 
+ * Copyright 2026 logoz <logoz@PROKOPII>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+
+int main(void) {
+    int a;
+	scanf("%d", &a);
+	
+	int hund = a / 100;          
+	int ten    = (a / 10) % 10;    
+	int unit   = a % 10; 
+	
+	int max = hund;
+	if (ten > hund) max = ten;
+	if (unit > hund) max = unit;
+	
+    printf ("%d\n", max);
+   
+   
+    return 0;
+}
+
+
